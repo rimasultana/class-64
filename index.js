94ece36814058bd4ffe934cfef17fbf1f251b0cb
rimasultana <rimasultana20225@gmail.com>
2024-03-15 01:03:21 +0600

    methods

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const person = {
+    firstName: "Rima",
+    lastName: "Sultana",
+    id: 5893,
+    fullName: function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+ console.log(person.fullName());
